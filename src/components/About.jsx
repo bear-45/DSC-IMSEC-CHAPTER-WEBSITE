9b79f7dbb699ff378f24a8293b5ff305c333af63
Pradumn Gaud <pradumngaurdadri@gmail.com>
2020-11-07 17:02:38 +0530

    'updatemember'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,26 @@
 import React from "react";
-import web from "../images/img3.svg";
+import '../components/css/About.css';
+import AboutNav from "../components/AboutNav";
+import AboutContent from "../components/AboutContent";
+import AboutHeader from "../components/Aboutheader"
 
 
-const About=()=>{
-
-
-
-return (
-    <>
-    <section id="header" className="d-flex align-item-center" >
-    <div className="container-fluid nav_bg">
-            <div className='row'>
-                <div className='col-10 mx-auto'>
-                    <div className="row">
-                    <div className="col-md-6 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                        <h1>Welcome to  <strong className="brand-name">DSC IMSEC CLUB</strong></h1>
-                        <h2 className="my-3">We are the team of talented developer making websites</h2>
+const About=()=> {
+        return (
+            <>
+                <section id="header" className="d-flex align-item-center flex-column">
+                    <AboutHeader/>
+                    <div className="container">
+                        <div className="row">
+                            <div id="navbar-example">
+                                <AboutNav/>
+                                <AboutContent/>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-lg-6 order-1 order-lg-2 header-img">
-                    <img  src={web} className="img-fluid1 animated"  width="90%" alt="home img"/>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    </>
-)
-
-};
+                </section>
+            </>
+        );
+    }
 
 export default About;
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,13 +1,36 @@
 import React from "react";
+import Team1 from "./MemberCard"; 
 
 
 const Team=()=>{
-
-
-
 return (
     <>
-    <h1>Hello Team</h1>
+    <div className="container-fluid">
+        <div className="row my-5" >
+            <Team1 className="col-12 d-flex justify-content-center align-items-center"/>
+        </div>
+        <div className="row my-5">
+            <Team1 className="col-3"/>
+            <Team1 className="col-3"/>
+            <Team1 className="col-3"/>
+            <Team1 className="col-3"/>
+        </div>
+        <div className="row my-5" >
+            <Team1 className="col-3"/>
+            <Team1 className="col-3"/>
+            <Team1 className="col-3"/>
+            <Team1 className="col-3"/>
+        </div>
+        <div className="row my-5" >
+            <Team1 className="col-4"/>
+            <Team1 className="col-4"/>
+            <Team1 className="col-4"/>
+        </div>
+        <div className="row my-5">
+            <Team1 className="col-3"/>
+            <Team1 className="col-3"/>
+        </div>
+    </div>
     </>
 )
 
